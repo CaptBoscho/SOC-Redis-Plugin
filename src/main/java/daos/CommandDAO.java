5d01c6acbfc95f2795f038f32d53c7ae40c591b4
Derek Argueta <darguetap@gmail.com>
2016-04-05 23:29:57 -0600

    inital project setup

---
+++ src/main/java/daos/CommandDAO.java
@@ -0,0 +1,55 @@
+package daos;
+
+import dto.IDTO;
+
+/**
+ * Created by Kyle 'TMD' Cornelison on 4/2/2016.
+ */
+public class CommandDAO implements ICommandDAO {
+
+    /**
+     * Handles adding a user,
+     * adding a command
+     * adding a game
+     *
+     * @param dto
+     */
+    @Override
+    public void addObject(IDTO dto) {
+
+    }
+
+    /**
+     * Handles verifying user which returns userID
+     * Getting the current game model
+     * getting a list of Commands
+     *
+     * @param dto
+     * @return
+     */
+    @Override
+    public IDTO readData(IDTO dto) {
+        return null;
+    }
+
+    /**
+     * mostly be used for updating the game blob state
+     *
+     * @param dto
+     */
+    @Override
+    public void updateData(IDTO dto) {
+
+    }
+
+    /**
+     * Mostly be used for deleting commands every n
+     * moves.
+     *
+     * @param dto
+     */
+    @Override
+    public void deleteData(IDTO dto) {
+
+    }
+}
---
+++ src/main/java/main/CLI.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ * @author Derek Argueta
+ *
+ * A class for manually interacting with Redis.
+ */
+public class CLI {
+
+    public static void main(String[] args) {
+
+    }
+}
