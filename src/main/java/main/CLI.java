7dba790b14655804eaffb36c7d7f90f997152ae5
Derek Argueta <darguetap@gmail.com>
2016-04-09 22:36:56 -0600

    redis instance works

--- src/main/java/main/CLI.java
+++ src/main/java/main/CLI.java
@@ -1,5 +1,7 @@
 package main;
 
+import redis.clients.jedis.Jedis;
+
 /**
  * @author Derek Argueta
  *
@@ -8,6 +10,7 @@ package main;
 public class CLI {
 
     public static void main(String[] args) {
-
+        Jedis jedis = new Jedis("45.55.178.18", 6379);
+        jedis.set("hi", "hello");
     }
 }
